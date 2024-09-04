d9ed9a0deb38d47f82b4828f5bd38bed68f40b9b
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2024-09-04 10:13:13 +0200

    update

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "iamcco/mardown-preview.nvim", enabled = false },
   { "stevearc/dressing.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
+  { "okuuva/auto-save.nvim", enabled = false },
 }
