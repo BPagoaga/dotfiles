7b153a43997d7a15f5557c24cf20250c64fd5464
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2025-02-19 19:30:01 +0100

    remove neotree and bufferline

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,16 +1,7 @@
 return {
-  { "akinsho/bufferline.nvim", enable = false },
+  { "akinsho/bufferline.nvim", enabled = false },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    enable = false,
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-      },
-    },
+    enabled = false,
   },
 }
